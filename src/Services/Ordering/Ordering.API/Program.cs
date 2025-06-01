e6fffb3f9b4cdbfd84210eda207da78565cdcfe8
ars12345 <arsalan2106@hotmail.com>
2025-05-31 21:43:11 -0400

    Ordering service

---
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -0,0 +1,23 @@
+using Ordering.API;
+using Ordering.Application;
+using Ordering.Infrastructure;
+using Ordering.Infrastructure.Data.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
+
+//Add services to container
+builder.Services
+    .AddApplicationServices()
+    .AddInfrastructureServices(builder.Configuration)
+    .AddApiServices(builder.Configuration);
+var app = builder.Build();
+
+//Configure HTTP request pipeline
+app.UseApiServices();
+
+if (app.Environment.IsDevelopment())
+{
+    await app.InitializeDatabaseAsync();
+}
+
+app.Run();
---
+++ src/Services/Ordering/Ordering.Infrastructure/Data/Interceptors/AuditableEntityInterceptor.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Ordering.Domain.Abstractions;
+
+namespace Ordering.Infrastructure.Data.Interceptors
+{
+    public class AuditableEntityInterceptor : SaveChangesInterceptor
+    {
+        public override InterceptionResult<int> SavingChanges(DbContextEventData eventData, InterceptionResult<int> result)
+        {
+            UpdateEntities(eventData.Context);
+            return base.SavingChanges(eventData, result);
+        }
+
+        public override ValueTask<InterceptionResult<int>> SavingChangesAsync(DbContextEventData eventData, InterceptionResult<int> result, CancellationToken cancellationToken = default)
+        {
+            UpdateEntities(eventData.Context);
+            return base.SavingChangesAsync(eventData, result, cancellationToken);
+        }
+
+        private void UpdateEntities(DbContext? context)
+        {
+            if (context == null) return;
+
+            foreach (var entry in context.ChangeTracker.Entries<IEntity>())
+            {
+                if (entry.State == EntityState.Added)
+                {
+                    entry.Entity.CreatedBy = "ok";
+                    entry.Entity.CreatedAt = DateTime.Now;
+                }
+                if (entry.State == EntityState.Added || entry.State == EntityState.Modified || entry.HasChangedOwnedEntities())
+                {
+                    entry.Entity.LastModifiedBy = "ok";
+                    entry.Entity.LastModified = DateTime.Now;
+                }
+            }
+        }
+    }
+
+    public static class Extensions
+    {
+        public static bool HasChangedOwnedEntities(this EntityEntry entry) =>
+            entry.References.Any(r =>
+                r.TargetEntry != null &&
+                r.TargetEntry.Metadata.IsOwned() &&
+                (r.TargetEntry.State == EntityState.Added || r.TargetEntry.State == EntityState.Modified));
+    }
+}
+
